7ed6416b0490fbc5a2f14d3cb92bffeeed3d7d8d
ansernawaz2012 <ansernawaz2012@gmail.com>
2019-05-02 17:21:19 +0100

    Add project files.

---
+++ CyberPet/Class1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CyberPet
+{
+    public class Pet
+    {
+        public Pet(string name)
+        {
+            PetName = name;
+          }
+        
+
+
+        public string PetName { get; set; }
+
+        public int Age { get; set; } = 1;
+        public int Hunger { get; set; } = 20;
+        public int Boredem { get; set; } = 20;
+        public int Fatigue { get; set; } = 20;
+
+        public void eat ()
+        {
+            if (Hunger> 0)
+            {
+                Hunger -= 10;
+            }
+        }
+
+        public void sleep()
+        {
+            if (Fatigue >= 50)
+            {
+                Fatigue -= 50;
+            
+            } else
+            {
+                Fatigue = 0;
+            }
+        }
+
+        public void play()
+        {
+            Boredem -= 10;
+        }
+
+        public void rest()
+        {
+            if (Fatigue >= 20)
+            {
+                Fatigue -= 20;
+
+            }
+            else
+            {
+                Fatigue = 0;
+            }
+        }
+
+        public void showState()
+        {
+            Console.WriteLine($"Pet Name: {PetName}");
+            Console.WriteLine($"Pet Hunger: {Hunger}");
+            Console.WriteLine($"Pet Boredem: {Boredem}");
+            Console.WriteLine($"Pet Fatigue: {Fatigue}");
+
+
+        }
+
+    }
+
+    
+
+
+
+
+    
+}
---
+++ CyberPet/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CyberPet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pet myPet = new Pet("Roger");
+
+            myPet.showState();
+
+            myPet.eat();
+            myPet.play();
+            myPet.sleep();
+            myPet.showState();
+            Console.ReadLine();
+        }
+    }
+}
