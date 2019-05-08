d527b019b7fc78d5b3b7593bb93ffe641bf612eb
ansernawaz2012 <ansernawaz2012@gmail.com>
2019-05-08 15:12:14 +0100

    Added a unit test

---
+++ CyberPetTests/PetTests.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CyberPet;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CyberPet.Tests
+{
+    [TestClass()]
+    public class PetTests
+    {
+        [TestMethod()]
+        public void petHungerTest()
+        {
+            //Arrange
+            var myPet = new Pet("Rover");
+            var hungerIncrease = 20;
+            myPet.Hunger = 70;
+            var expected = myPet.Hunger + hungerIncrease;
+            //Act
+            myPet.petHunger(hungerIncrease);
+            var actual = myPet.Hunger;
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
